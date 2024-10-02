0acffbf56b2d4f48858f855b3b84c562912719fb
crjostad <crjostad@students.unwsp.edu>
2024-10-02 18:55:04 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -15,6 +15,8 @@ return miles
 #### you only need to worry about the actual temp 
 #### conversion logic in the temp_conversion function
 if __name__ == '__main__':
-    # Get User Input
-    print('in main')
+   kilometers = float(input("Enter distance in kilometers: "))
+    miles = kilometer_conversion(kilometers)
+    print(f"{kilometers} kilometers is equal to {miles} miles.")
+
     
