dbbc1e3f377e9b54d1b1f9e9037361f0275295f1
crjostad <crjostad@students.unwsp.edu>
2024-10-02 21:35:36 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,11 +1,11 @@
-def kilometer_conversion():
+def kilometer_conversion(kilometers):
     miles = kilometers * 0.6214
     return miles
 
 if __name__ == '__main__':
     
     kilometers = float(input("Enter distance in kilometers: "))
-    miles = kilometer_conversion()
+    miles = kilometer_conversion(kilometers)
     print("It is", miles, "miles")
 
     
