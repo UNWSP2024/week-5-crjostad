1635a8df6e6ca78bf9b1af324944ecd4c622f110
crjostad <crjostad@students.unwsp.edu>
2024-10-03 10:34:53 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,17 +1,21 @@
 import random
 
-num1 = random.randint(1, 500)
-num2 = random.randint(1, 500)
+def quiz_numbers():
+    num1 = random.randint(1, 500)
+    num2 = random.randint(1, 500)
+    return num1, num2
+
+def check_answer(num1, num2, answer):
+    if answer == num1 + num2:
+        print("Congratulations")
+    else:
+        print("You are incorrect the answer is", num1 + num2)
+
+num1, num2 = quiz_numbers()
 
 print(num1)
 print("  +")
 print(num2)
-
-
+print("___")
 answer = int(input("Enter answer: "))
-
-if answer == num1 + num2:
-    print("Congratulations")
-
-if answer != num1 + num2:
-    print("You are incorrect the answer is", num1 + num2)
+check_answer(num1, num2, answer)
